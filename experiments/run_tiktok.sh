6a41d6c59e9c6b12c949cd4cf4da2eb41ed34f03
ITKOO <itkoo2000@gmail.com>
2025-04-22 05:17:15 +0000

    Add TikTok workload benchmark script (frequent write, read-after-write)

---
+++ experiments/run_tiktok.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WBS_LIST=(16777216 33554432 67108864)
+TRIGGER_LIST=(4 8 16)
+RESULT_DIR="./results/tiktok"
+mkdir -p $RESULT_DIR
+
+for WBS in "${WBS_LIST[@]}"; do
+  for TR in "${TRIGGER_LIST[@]}"; do
+    ../db_bench \
+      --db=/tmp/tiktok_wbs${WBS}_tr${TR} \
+      --benchmarks=fillrandom,readwhilewriting \
+      --num=20000 \
+      --value_size=256000 \
+      --write_buffer_size=$WBS \
+      --level0_file_num_compaction_trigger=$TR \
+      --compression_type=snappy \
+      --compaction_style=0 \
+      --statistics \
+      > $RESULT_DIR/result_wbs${WBS}_tr${TR}.log
+  done
+done
+
