7717e842f03e228f420d2b1b9238ec2892bd15e5
Ubuntu <ubuntu@ip-172-31-9-192.us-east-2.compute.internal>
2025-04-22 05:16:38 +0000

    Add KakaoTalk workload benchmark script (frequent write, recent read)

---
+++ experiments/run_kakao.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+WBS_LIST=(8388608 16777216 33554432)      # 8MB, 16MB, 32MB
+TRIGGER_LIST=(4 8 16)
+RESULT_DIR="./results/kakao"
+mkdir -p $RESULT_DIR
+
+for WBS in "${WBS_LIST[@]}"; do
+  for TR in "${TRIGGER_LIST[@]}"; do
+    ../db_bench \
+      --db=/tmp/kakao_wbs${WBS}_tr${TR} \
+      --benchmarks=fillrandom,readlatest \
+      --num=500000 \
+      --value_size=100 \
+      --write_buffer_size=$WBS \
+      --level0_file_num_compaction_trigger=$TR \
+      --compression_type=snappy \
+      --compaction_style=0 \
+      --statistics \
+      > $RESULT_DIR/result_wbs${WBS}_tr${TR}.log
+  done
+done
+
